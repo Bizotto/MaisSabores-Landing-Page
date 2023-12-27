48ac1cc47f66a7e85b6575d830fe697df283890d
Nicolas Bizotto <bizotto.nicolas@gmail.com>
2023-12-27 01:56:02 -0300

    fix: new responsive features

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,39 +1,39 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Hero() {
-  return (
-    <section className="mx-auto flex h-[100vh] max-w-7xl items-center justify-center max-sm:mx-5 max-md:mx-5 max-lg:mx-5 max-xl:mx-5 ">
-      <div className="flex flex-col items-center justify-center gap-8 text-center max-sm:mt-48 max-[375px]:mt-24 md:items-start md:text-start">
-        <h1 className="text-9xl max-sm:text-6xl">
-          Empadas <br /> Amore
-        </h1>
+	return (
+		<section className="mx-auto flex h-[100vh] max-w-7xl items-center justify-center max-sm:mx-5 max-md:mx-5 max-lg:mx-5 max-xl:mx-5 ">
+			<div className="flex flex-col items-center justify-center gap-8 text-center max-sm:mt-24 max-[375px]:mt-24 md:items-start md:text-start">
+				<h1 className="text-9xl max-sm:text-6xl">
+					Empadas <br /> Amore
+				</h1>
 
-        <h2 className="text-2xl">
-          Venha conhecer os deliciosos sabores das empadas caseiras Amore!
-          <br /> Aqui, Criamos uma experiencia mais do que gastronômica. <br />
-        </h2>
+				<h2 className="text-2xl">
+					Venha conhecer os deliciosos sabores das empadas caseiras Amore!
+					<br /> Aqui, Criamos uma experiencia mais do que gastronômica. <br />
+				</h2>
 
-        <div className="flex flex-col items-center gap-5 text-xl md:flex-row">
-          <Link
-            className="border-tertiary rounded-2xl border p-4 text-xl transition-all duration-300 hover:scale-110 hover:bg-[#0001]"
-            href="/menu"
-          >
-            <p>Encomende!</p>
-          </Link>
-          <p>Feitinhos com muito Amor!!</p>
-        </div>
-      </div>
+				<div className="flex flex-col items-center gap-5 text-xl md:flex-row">
+					<Link
+						className="border-tertiary rounded-2xl border p-4 text-xl transition-all duration-300 hover:scale-110 hover:bg-[#0001]"
+						href="/menu"
+					>
+						<p>Encomende!</p>
+					</Link>
+					<p>Feitinhos com muito Amor!!</p>
+				</div>
+			</div>
 
-      <div>
-        <Image
-          alt="Imagem contendo a logo das empadas Amore"
-          src="https://picsum.photos/800"
-          width={600}
-          height={800}
-          className="hidden rounded-3xl shadow-md shadow-black/50 lg:block"
-        />
-      </div>
-    </section>
-  );
+			<div>
+				<Image
+					alt="Imagem contendo a logo das empadas Amore"
+					src="https://picsum.photos/800"
+					width={600}
+					height={800}
+					className="hidden rounded-3xl shadow-md shadow-black/50 lg:block"
+				/>
+			</div>
+		</section>
+	)
 }
