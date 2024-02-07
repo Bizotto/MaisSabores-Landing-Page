62b8cbdd310630c97ae8b11e2bb3f7dfcfc90a4e
Nicolas Bizotto <bizotto.nicolas@gmail.com>
2024-02-06 23:33:47 -0300

    fix: fix instagram link

--- src/components/floatingButton.tsx
+++ src/components/floatingButton.tsx
@@ -8,10 +8,7 @@ export default function FloatingButton() {
 
   return (
     <div className="lg:hidden xl:hidden md:hidden 2xl:hidden fixed flex shadow-md shadow-black/30 justify-between gap-4 bg-secondary-200 z-50 bottom-3 right-4 rounded-full border-2 p-2 border-secondary-300">
-      <Link
-        href="https://www.instagram.com/empadamores_?igshid=OGQ5ZDc2ODk2ZA=="
-        target="_blank"
-      >
+      <Link href="https://www.instagram.com/empadamaissabores/" target="_blank">
         <div className="border-2 border-secondary-300 p-2 rounded-full">
           <InstagramIcon className="h-6 w-6 text-secondary-300" />
         </div>
