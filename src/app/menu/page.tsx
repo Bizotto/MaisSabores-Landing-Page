be6b82a9f889482081a4dbbf67b009b9ec01e01b
Nicolas Bizotto <bizotto.nicolas@gmail.com>
2024-04-13 13:56:00 -0300

    fix: resposive view height

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import { empadas, empadinhas, escondidnho } from "../../components/constants";
 export default function Menu() {
   return (
     <div className="max-sm:flex flex-col gap-20 max-sm:mx-5 max-md:mx-5 max-lg:mx-5 max-xl:mx-5">
-      <section className="mx-auto flex-col flex max-w-7xl h-[100vh] max-[375px]:mt-48 max-sm:mt-48 items-center justify-center space-y-5">
+      <section className="mx-auto flex-col flex max-w-7xl h-[100vh] items-center justify-center space-y-5">
         <h1 className="text-6xl font-alt flex items-center g justify-center max-md:mt-24">
           Cardápio
         </h1>
@@ -25,7 +25,7 @@ export default function Menu() {
           })}
         </div>
       </section>
-      <section className="mx-auto flex-col flex max-w-7xl h-[100vh] max-[375px]:mt-48 max-sm:mt-48 items-center justify-center space-y-5">
+      <section className="mx-auto flex-col flex max-w-7xl h-[100vh] items-center justify-center space-y-5">
         <h2 className="text-3xl font-alt flex items-center justify-center">
           Empadinhas
         </h2>
@@ -43,7 +43,7 @@ export default function Menu() {
           })}
         </div>
       </section>
-      <section className="mx-auto flex-col flex max-w-7xl h-[100vh] max-[375px]:mt-48 max-sm:mt-48 items-center justify-center space-y-5">
+      <section className="mx-auto flex-col flex max-w-7xl items-center justify-center space-y-5">
         <h2 className="text-3xl font-alt flex items-center justify-center">
           Escondidnhos
         </h2>
