e8d43d87b260bdcc377c55c0a0932601bc38c3e7
Nicolas Bizotto <bizotto.nicolas@gmail.com>
2024-05-21 14:32:43 -0300

    alterei o hero da minha aplicacao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Hero from "@/components/Hero";
 import Service from "@/components/Service";
 
 export default function Home() {
   return (
     <main className="max-sm:mx-4">
-      <Hero />
       <div className="flex gap-10 flex-col">
         <Service />
       </div>
